f2ed9424558556a4dbc9f67028923c1e23a2842e
Szilárd Kósa <kszilard777@gmail.com>
2021-04-10 15:16:41 +0200

    Views added

--- app/src/main/java/com/example/moblabhw/view/details/DetailsActivity.kt
+++ app/src/main/java/com/example/moblabhw/view/details/DetailsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.moblabhw
+package com.example.moblabhw.view.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moblabhw.R
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
     }
 }
